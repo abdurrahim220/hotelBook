67aee0dcd79f056e466fd2d963bb8adecf4b457a
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-12-24 19:09:21 +0600

    header and footer design complete

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,11 +1,20 @@
+
+import Footer from "../components/Footer/Footer";
 import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
+import { Outlet } from 'react-router-dom';
+
+
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero/>
+      <Hero />
+     <div className="container mx-auto py-10 flex-1">
+      <Outlet/>
+     </div>
+      <Footer />
     </div>
   );
 };
